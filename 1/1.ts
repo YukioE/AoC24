79ea2cfce2967403b0a31ed47ef5caed9106e8be
YukioE <yukiod8@pm.me>
2024-12-01 21:57:39 +0100

    day 1 part 2 solved

--- 1/1.ts
+++ 1/1.ts
@@ -10,17 +10,34 @@ let list2 = fs
     .split("\n")
     .map((x) => parseInt(x.split("   ")[1]));
 
+// sort lists
 list1.sort((a, b) => a - b);
 list2.sort((a, b) => a - b);
 
 console.log(list1, list2);
 
+// calculate distances
 let distances = list1.map((x) => Math.abs(list2[list1.indexOf(x)] - x));
 console.log(distances);
 
+// calculate sum of distances
 let sum = 0;
 distances.forEach((x) => {
     sum += x
-    console.log(sum, x);
 });
-console.log(sum);
+
+console.log("sum:", sum);
+
+// calculate similarity
+let similarity = 0;
+list1.forEach(x => {
+    let count = 0;
+    list2.forEach(y => {
+        if (x === y) {
+            count++;
+        }
+    });
+    similarity += count * x;
+});
+
+console.log("similarity: ", similarity);
