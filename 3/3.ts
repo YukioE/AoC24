f805c4ff03de480ab4cb8ccb7ccb9d30451ff2d8
YukioE <yukiod8@pm.me>
2024-12-03 23:27:51 +0100

    added skills.md

--- 3/3.ts
+++ 3/3.ts
@@ -6,7 +6,7 @@ const part1regex = /mul\(\d{1,3},\d{1,3}\)/g;
 const rawMul = input.match(part1regex);
 
 let sum : number = 0;
-let mulNumbers = rawMul?.forEach((mul) => {
+rawMul?.forEach((mul) => {
     const [a, b] = mul.split("(")[1].split(")")[0].split(",")
     sum += parseInt(a) * parseInt(b);
 });
@@ -19,7 +19,7 @@ const rawMul2 = input.match(part2regex);
 
 let sum2 : number = 0;
 let enabled = true;
-let mulNumbers2 = rawMul2?.forEach((mul) => {
+rawMul2?.forEach((mul) => {
     if (mul === "don't()") {
         enabled = false;
     } else if (mul === "do()") {
---
+++ skills.md
@@ -0,0 +1,5 @@
+|day|skill|
+|:-:|:-:|
+|1|reading input<br>Array methods|
+|2|-|
+|3|matching regex|
